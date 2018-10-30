a90f91926a9f0df6873576e51f78d416ad7bb7e6
Deja Trudeaux <dejadej@gmail.com>
2018-10-30 12:05:13 -0500

    added airbnb eslint and some directory tree

---
+++ angular-client/components/app.js
@@ -0,0 +1,10 @@
+angular.module('app')
+  .controller('AppCtrl', () => {
+   
+  })
+  .component('app', {
+    bindings: {
+    },
+    controller: 'AppCtrl',
+    templateUrl: '/templates/app.html',
+  });
